3dcaf379e53f2599446f2d9a2773de0bfc7f5e88
Warren Fernandes <warren.f.fernandes@gmail.com>
2015-03-23 22:54:56 -0600

    Adding test harness

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import (
+	. "github.com/wfernandes/golife"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Main", func() {
+
+	Context("should spin up a web server", func() {
+
+	})
+})
